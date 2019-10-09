d87ec80d4daeda4916bdee00b4afb93967151f1e
jlozovei <jlozovei@gmail.com>
2019-10-09 18:44:35 -0300

    fix twitter share options

--- src/js/index.js
+++ src/js/index.js
@@ -44,8 +44,14 @@ import '../css/index.css';
     },
 
     generateShareLink(title) {
+      const options = {
+        username: 'juliolozovei',
+        hashtags: 'developer,developertitles',
+        url: 'https://developertitles.com/'
+      };
+
       return encodeURI(
-        `https://twitter.com/intent/tweet?url=https://developertitles.com/&text=I'm the "${title}". Check your title at Developer Titles&related=jlozovei&hashtags=developer,developertitles`
+        `https://twitter.com/intent/tweet?url=${options.url}&text=I'm the "${title}". Check your title at Developer Titles&related=${options.username}&hashtags=${options.hashtags}`
       );
     },
 
